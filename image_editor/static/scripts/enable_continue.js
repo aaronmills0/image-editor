09662fd8bc303e89f111a1c37756e1bde08fe4f4
Aaron Mills <74072722+aaronmills0@users.noreply.github.com>
2021-08-14 23:07:14 -0400

    handle removal of image file

--- image_editor/static/scripts/enable_continue.js
+++ image_editor/static/scripts/enable_continue.js
@@ -1,3 +1,12 @@
 function enableContinue() {
     document.getElementById("continue-button").classList.remove("btn-custom-disabled");
 }
+
+function disableContinue() {
+    var file_path = document.getElementById('id_image').value;
+    let file_name = file_path.substring(12);
+    if (file_name === "") {
+        document.getElementById('upload-text').innerHTML = "Upload"
+        document.getElementById("continue-button").classList.add("btn-custom-disabled");
+    }
+}
