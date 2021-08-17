10ec194e2256e829cf70c556908dbc15e2b53aff
Aaron Mills <74072722+aaronmills0@users.noreply.github.com>
2021-08-17 16:25:09 -0400

    tmp is cleared when canvas is exited

--- image_editor/editor/models.py
+++ image_editor/editor/models.py
@@ -4,7 +4,5 @@ import os
 from django.conf import settings
 
 class Image(models.Model):
-    shutil.rmtree('{}\\tmp'.format(settings.MEDIA_ROOT[:-1]))
-    os.mkdir('{}\\tmp'.format(settings.MEDIA_ROOT[:-1]))
     image = models.ImageField(upload_to='tmp')
 
--- image_editor/editor/views.py
+++ image_editor/editor/views.py
@@ -4,10 +4,26 @@ from .forms import ImageForm
 import cv2 as cv
 import numpy as np
 from PIL import Image
+import os, shutil
+from django.conf import settings
 # Create your views here.
 img_name = None;
 
+def clear_tmp():
+    folder = settings.MEDIA_ROOT + 'tmp'
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print("Failed to clear /tmp: {}".format(e))
+
 def image_upload(request):
+    clear_tmp()
+    global img_name
     print(request.method)
     template_name ='index.html'
     if request.method == 'POST':
@@ -17,7 +33,8 @@ def image_upload(request):
             print(form)
             print(img)
             img.save()
-            img_name = img
+            img_name = form.cleaned_data.get('image')
+            print(img_name)
             return redirect('/canvas/')
     else:
         form = ImageForm()
@@ -26,4 +43,5 @@ def image_upload(request):
 
 def canvas(request):
     template_name = 'canvas.html'
+    print('Name: {}'.format(img_name))
     return render(request, template_name, {'name': img_name})
