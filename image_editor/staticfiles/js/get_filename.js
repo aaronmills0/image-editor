acc856e570fedacc66f87afea755788c15495a39
Aaron Mills <74072722+aaronmills0@users.noreply.github.com>
2021-08-22 22:07:15 -0400

    removed staticfiles directory

--- image_editor/staticfiles/js/get_filename.js
+++
@@ -1,7 +0,0 @@
-
-
-function getFilename() {
-    var file_path = document.getElementById('id_image').value;
-    let file_name = file_path.substring(12);
-    document.getElementById('upload-text').innerHTML = file_name;
-}
--- image_editor/staticfiles/js/toggle_slider.js
+++
@@ -1,9 +0,0 @@
-function toggleSlider(id) {
-    var cur = document.getElementById(id);
-    if (cur.value == "1") {
-        cur.value = "0";
-    } else {
-        cur.value = "1";
-    }
-    location.reload();
-}
